d116ae209d42d7dc3af258bde138ab0bcf762e36
James W. Barnett <wes@wbarnett.us>
2014-07-16 21:53:38 -0500

    changed read to saved

--- src/Gmx.jl
+++ src/Gmx.jl
@@ -169,7 +169,7 @@ function read_gmx(xtc_file::String,first::Int=1,last::Int=100000,skip::Int=1,
 
     end 
 
-    println(char(13),"Read in ", no_frames, " frames.")
+    println(char(13),"Saved ", no_frames, " frames.")
 
     # Resize the arrays
     box_array = fill!(Array(Any,no_frames),Array(Float32,(3,3)))
