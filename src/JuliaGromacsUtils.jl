d50f29d640610194748aa1e376838d87cca7cdfb
James W. Barnett <wes@wbarnett.us>
2014-07-17 11:58:00 -0500

    tying to figure out this package thing

--- src/JuliaGromacsUtils.jl
+++ src/JuliaGromacsUtils.jl
@@ -1,5 +1,10 @@
 module JuliaGromacsUtils
 
+export Gmx: read_gmx
+export Xtc: xtc_init, read_xtc, close_xtc
+export Ndx: read_ndx
+export Utils: pbc
+
     include("Xtc.jl")
     include("Ndx.jl")
     include("Gmx.jl")
