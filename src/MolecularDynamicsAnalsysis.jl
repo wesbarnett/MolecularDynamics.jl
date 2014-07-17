845d840aec26593627a2a170d2714f38afdbbe37
James W. Barnett <wes@wbarnett.us>
2014-07-17 12:32:37 -0500

    remove exports

--- src/MolecularDynamicsAnalsysis.jl
+++ src/MolecularDynamicsAnalsysis.jl
@@ -1,12 +1,5 @@
 module JuliaGromacsUtils
 
-export xtc_init, 
-       read_xtc, 
-       close_xtc,
-       read_ndx,
-       read_gmx,
-       pbc
-
     include("Xtc.jl")
     include("Ndx.jl")
     include("Gmx.jl")
