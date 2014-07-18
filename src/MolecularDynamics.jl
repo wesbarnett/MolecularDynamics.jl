1d4c979d19d236f3e8100dfaefc42224b45f88eb
James W. Barnett <wes@wbarnett.us>
2014-07-18 14:06:50 -0500

    make top level functions

--- src/MolecularDynamics.jl
+++ src/MolecularDynamics.jl
@@ -1,5 +1,19 @@
 module MolecularDynamics
 
+import .Xtc: xtc_init, read_xtc, close_xtc
+import .Ndx: read_ndx
+import .Gmx: read_gmx
+import .Utils: pbc, bond_angle, dih_angle
+
+export read_gmx, 
+	   xtc_init, 
+	   read_xtc, 
+	   close_xtc,
+	   read_ndx,
+	   pbc,
+	   bond_angle,
+	   dih_angle
+
     include("Xtc.jl")
     include("Ndx.jl")
     include("Gmx.jl")
