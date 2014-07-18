50738ed8e58d63ba56c4be91c003faa9d74d9153
James W. Barnett <wes@wbarnett.us>
2014-07-18 13:41:37 -0500

    revert previous change

--- src/MolecularDynamics.jl
+++ src/MolecularDynamics.jl
@@ -1,14 +1,5 @@
 module MolecularDynamics
 
-export read_gmx,
-       xtc_init,
-       read_xtc,
-       close_xtc,
-       read_ndx,
-       dih_angle,
-       bond_angle,
-       pbc
-
     include("Xtc.jl")
     include("Ndx.jl")
     include("Gmx.jl")
