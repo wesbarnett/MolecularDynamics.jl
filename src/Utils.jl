18242115f2198090142f807fa547895d876b39f3
James W. Barnett <wes@wbarnett.us>
2014-07-18 21:44:40 -0500

    box_vol: export

--- src/Utils.jl
+++ src/Utils.jl
@@ -8,7 +8,8 @@ module Utils
 export pbc, 
        dih_angle,
 	   bond_angle,
-       rdf
+       rdf,
+	   box_vol
 
 # Adjusts for periodic boundary condition. Input is a three-dimensional
 # vector (the position) and the box ( 3 x 3 Array). A 3d vector is returned.
