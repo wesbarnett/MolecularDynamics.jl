4f84e91e78dedf01c054aaa708dfeef18e62cdb3
James W. Barnett <wes@wbarnett.us>
2014-07-21 12:05:35 -0500

    travis.yml: fix float comparison

--- test/read_test.jl
+++ test/read_test.jl
@@ -1,11 +1,15 @@
 
 using MolecularDynamics
 
-gmx = read_gmx("../examples/gmx-test/traj.xtc","../examples/gmx-test/index.ndx","C")
+gmx = read_gmx("examples/gmx-test/traj.xtc","examples/gmx-test/index.ndx","C")
 
-if (gmx.x["C"][2][1,3] == 1.315 &
-	gmx.natoms["4"] == 4 &
-	gmx.no_frames == 101)
+test = Array(Bool,3)
+
+test[1] = isequal(string(gmx.x["C"][2][1,3]),"1.315")
+test[2] = isequal(gmx.natoms["C"],4)
+test[3] = isequal(gmx.no_frames,101)
+
+if (all(test))
 	stat = 0
 else
 	stat = 1
