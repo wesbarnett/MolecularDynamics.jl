1f108d7a337c5071b25c6d8077c2d32e9667985a
James W. Barnett <wes@wbarnett.us>
2014-07-21 14:36:00 -0500

    dih_angle: add alternative calls

--- src/Utils.jl
+++ src/Utils.jl
@@ -157,6 +157,12 @@ function dih_angle(a::Array{Float32,2},box::Array{Float32,2})
 
 end
 
+function dih_angle(gmx,group::String,frame::Int)
+
+    return dih_angle(gmx.x[group][frame],gmx.box[frame])
+
+end
+
 # Cycles through all frames
 function dih_angle(f::Array{Any,1},box::Array{Any,1})
 
@@ -171,6 +177,12 @@ function dih_angle(f::Array{Any,1},box::Array{Any,1})
 	end
 
     return angles
+
+end
+
+function dih_angle(gmx,group::String)
+
+    return dih_angle(gmx.x[group],gmx.box)
 
 end
 
