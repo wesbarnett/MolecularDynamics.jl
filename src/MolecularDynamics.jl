5b053541e3da755460ea2dfd285e3c5eafd051c9
James W. Barnett <wes@wbarnett.us>
2014-07-21 08:42:08 -0500

    fixing modules

--- src/MolecularDynamics.jl
+++ src/MolecularDynamics.jl
@@ -1,10 +1,5 @@
 module MolecularDynamics
 
-using .Xtc
-import .Ndx: read_ndx
-import .Gmx: read_gmx
-import .Utils: pbc, bond_angle, dih_angle, rdf, prox_rdf, box_vol
-
 export read_gmx, 
 	   xtc_init, 
 	   read_xtc, 
@@ -21,5 +16,10 @@ include("Xtc.jl")
 include("Ndx.jl")
 include("Gmx.jl")
 include("Utils.jl")
+
+using .Xtc
+using .Ndx
+using .Gmx
+using .Utils
 
 end
